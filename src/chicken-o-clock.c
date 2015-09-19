fa36679e7d19880c54a222262e6cbd8a46b845cc
dvberkel <daan.v.berkel.1980@gmail.com>
2015-09-19 18:33:45 +0200

    Create an initial blank window

--- src/chicken-o-clock.c
+++ src/chicken-o-clock.c
@@ -1,9 +1,26 @@
 #include <pebble.h>
 
+static Window *main_window;
+
+static void main_window_load(){
+}
+
+static void main_window_unload(){
+}
+
 static void init(){
+  main_window = window_create();
+
+  window_set_window_handlers(main_window, (WindowHandlers) {
+      .load   = main_window_load,
+      .unload = main_window_unload
+  });
+
+  window_stack_push(main_window, true);
 }
 
 static void deinit(){
+  window_destroy(main_window);
 }
 
 int main(void){
